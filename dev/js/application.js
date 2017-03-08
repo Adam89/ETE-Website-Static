5ce6d698f27c74cce0399f98e9f9edb0033a7563
Adam Hopkins <adamhopkins87@gmail.com>
2017-03-08 22:16:44 +0000

    started social feed

--- dev/js/application.js
+++ dev/js/application.js
@@ -2,8 +2,16 @@
 global.jQuery = require('jquery');
 global.$ = jQuery;
 
+
+//frameworks
+
+require('./modules/dot.min.js')();
+require('./modules/jquery.socialfeed.js')();
+require('./modules/moment.js')();
+
 // Modules
 var nav = require('./modules/nav')();
+var nav = require('./modules/social')();
 
 // Bootstrapping
 $(function() {
