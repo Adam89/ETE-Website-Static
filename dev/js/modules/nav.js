bb5e85e0dc6afce32394cb70bbba77ff365fd4f7
Adam Hopkins <adamh@khws.co.uk>
2017-04-18 17:41:47 +0100

    added in mobile views and video swap JS

--- dev/js/modules/nav.js
+++ dev/js/modules/nav.js
@@ -7,19 +7,26 @@ function nav() {
 
     nav.init = function() {
 
-        var hamburger = document.querySelector('.wrapper__hamburger');
-        var mobileNav = document.querySelector('.mobileNav');
+        // var hamburger = document.querySelector('.wrapper__hamburger');
+        // var mobileNav = document.querySelector('.mobileNav');
 
-        hamburger.addEventListener('click', function() {
-            if (mobileNav.style.display == 'none') {
-                event.preventDefault();
-                mobileNav.style.display = 'block';
-            } else {
-                event.preventDefault();
-                mobileNav.style.display = 'none';
-            }
-        });
+        // hamburger.addEventListener('click', function() {
+        //     if (mobileNav.style.display == 'none') {
+        //         event.preventDefault();
+        //         mobileNav.style.display = 'block';
+        //     } else {
+        //         event.preventDefault();
+        //         mobileNav.style.display = 'none';
+        //     }
+        // });
 
+$(document).ready(function(){
+    $("a").click(function(e) {
+        e.preventDefault();
+
+        $("#someFrame").attr("src", $(this).attr("href"));
+    });
+});
 
     };
 
