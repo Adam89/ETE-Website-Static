4d55fecaaef57df64715bcbc18c28d20246f8682
Adam Hopkins <adamh@khws.co.uk>
2017-06-16 17:14:52 +0100

    more react stuff

--- Udemy/ReduxSimpleStarter/src/index.js
+++ Udemy/ReduxSimpleStarter/src/index.js
@@ -9,7 +9,10 @@ All components are child's of the root container div within the DOM.
 Always one component per file. Make sure you break up your app into individual items.
 
 A class component is used when we want a component to have internal record keeping ability to be aware of itself and whats happened to it when its rendered;
-*/
+
+Functional component the props object is an argument
+
+Class props are available in any method we define by using this.props keep in my mind when refactoring functinal components to class based
 
 /*
 
@@ -32,7 +35,7 @@ import React, { Component } from 'react'; // go find library and assign it to Re
 import ReactDOM from 'react-dom'; // react dom is library for rendering JSX to the DOM
 import YTSearch from 'youtube-api-search'; //package
 import SearchBar from './components/searchbar';
-
+import VideoList from './components/videoList';
 
 const youtube_Api_Key = 'AIzaSyBsj2cI1pDVoTdWrojxyp6AHhQ5Sm6wxtQ';
 
@@ -58,7 +61,7 @@ class App extends Component { // app needs to keep track of list of videos so it
         YTSearch({
             key: youtube_Api_Key,
             term: 'surfboards'
-        }, (videos) => {
+        }, (videos) => {// we also pass data or pass props to video list below
             this.setState({ videos }); // es6 allows use just videos when key and property parameter we are passing are the same
             console.log(videos);
         }); // fetched youtube array of surfboards
@@ -66,7 +69,8 @@ class App extends Component { // app needs to keep track of list of videos so it
     render() {
         return ( < div >
             < SearchBar / >
-            < /div>
+            <VideoList videos={this.state.videos}/>
+            </div>
         );
     }
 }
