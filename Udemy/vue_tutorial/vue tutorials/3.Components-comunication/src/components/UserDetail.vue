2cf815d90cee23c7401b2f7d9a45304ab1edaa8e
Adam Hopkins <adamh@khws.co.uk>
2017-07-19 17:33:49 +0100

    tidiyed up code plus added vue tutorials to project

---
+++ Udemy/vue_tutorial/vue tutorials/3.Components-comunication/src/components/UserDetail.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+    </div>
+</template>
+
+<script>
+
+export default { // this component is going to receive some data (PROPS from outside props is an object)
+	props: ['name']
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
